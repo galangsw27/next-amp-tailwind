c066f0f84c2ab1727b1756620a58aeb7ec682d2c
galangsatria27 <galangiphone27@gmail.com>
2024-08-03 04:36:44 +0700

    update amp=img jsx

--- src/amp.d.ts
+++ src/amp.d.ts
@@ -1,9 +1,27 @@
-// src/amp.d.ts
+// Any element you create will be accepted
 declare namespace JSX {
-    interface IntrinsicElements {
-      'amp-img': React.DetailedHTMLProps<React.ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement>;
-      // Add other AMP elements here as needed
-    
-    }
-  }
-  
+	interface IntrinsicElements {
+		[elemName: string]: any;
+	}
+}
+
+// The elements you list here will be accepted, attributes don't matter
+declare namespace JSX {
+	interface IntrinsicElements {
+		'amp-img': any;
+	}
+}
+
+// The elements you list here will be accepted, and only with the attributes that you include here
+declare namespace JSX {
+	interface AmpImg {
+		alt?: string;
+		src?: string;
+		width?: string;
+		height?: string;
+		layout?: string;
+	}
+	interface IntrinsicElements {
+		'amp-img': AmpImg;
+	}
+}
