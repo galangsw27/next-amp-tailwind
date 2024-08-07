a187a779131c9928206fbf86945a8af328707c7d
galangsatria27 <galangiphone27@gmail.com>
2024-08-07 23:21:17 +0700

    changeUrl

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: 'http://kedanjosss88.lol',
+    siteUrl: 'https://kedanjosss88.lol',
     generateSitemap: true,
     generateRobotsTxt: true, 
     sitemapSize: 7000,
